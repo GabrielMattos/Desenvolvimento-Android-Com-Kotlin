e6239347b9d494bb2e01192d59be6e45acf62e0a
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-08 20:11:16 -0300

    019 Adicionando evento de clique em um botão

--- PrimeiroProjetodoCurso/app/src/main/java/cursoandroid/com/br/MainActivity.kt
+++ PrimeiroProjetodoCurso/app/src/main/java/cursoandroid/com/br/MainActivity.kt
@@ -2,11 +2,19 @@ package cursoandroid.com.br
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun CliqueBotao(view: View)
+    {
+        var texto = findViewById<TextView>(R.id.id_textoExibicao);
+        texto.setText("Texto Alterado");
     }
 }
