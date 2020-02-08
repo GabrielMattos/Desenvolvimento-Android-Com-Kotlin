22cc7a98079b5226f7b31aace0344c5e1ae56397
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-08 19:24:43 -0300

    016 Criando meu primeiro projeto

---
+++ PrimeiroProjetodoCurso/app/src/main/java/cursoandroid/com/br/MainActivity.kt
@@ -0,0 +1,12 @@
+package cursoandroid.com.br
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
