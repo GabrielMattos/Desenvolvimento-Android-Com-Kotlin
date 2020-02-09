935a5c047031eaa74d8795ec04c7fae0cd6666b5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-09 13:36:31 -0300

    036 Criando interface App2 Frases do dia

---
+++ Frasesdodia/app/src/main/java/cursoandroid/com/frasesdodia/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package cursoandroid.com.frasesdodia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
