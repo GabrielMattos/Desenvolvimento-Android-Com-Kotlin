08eae994192d54d6f0b51021fbbc39b4cd98b732
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-09 13:11:36 -0300

    021 Escrevendo os códigos do App1 Sorteio

--- Sorteio/app/src/main/java/cursoandroid/com/sorteio/MainActivity.kt
+++ Sorteio/app/src/main/java/cursoandroid/com/sorteio/MainActivity.kt
@@ -2,11 +2,21 @@ package cursoandroid.com.sorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonSelecionarNumero(view: View)
+    {
+        var numeroSorteado = Random.nextInt(10, 100)
+        var txt = findViewById<TextView>(R.id.id_textViewNumeroSelecionado)
+        txt.setText("Número sorteado é: $numeroSorteado")
     }
 }
