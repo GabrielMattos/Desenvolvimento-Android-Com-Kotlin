6e0e012dee00319eddab01b80abf806d321b5e38
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-13 01:01:26 -0300

    039 Finalizando App2 Frases do dia - Parte 2

--- Frasesdodia/app/src/main/java/cursoandroid/com/frasesdodia/MainActivity.kt
+++ Frasesdodia/app/src/main/java/cursoandroid/com/frasesdodia/MainActivity.kt
@@ -3,11 +3,30 @@ package cursoandroid.com.frasesdodia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var texto : TextView
+    val frases = arrayOf(
+        "hjvgjsvghvjsghjgjsdhjvgsjhgsh",
+        "efifjeiofujeiofeioufeufeofeiefoi",
+        "fefeuiefiufuiyefiuyefiuyfeiefpwdiodiwpdwio"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.id_textViewFrase)
+    }
+
+    fun gerarFrase(view: View)
+    {
+        val totalItensArray = frases.size
+        val numeroAleatorio = Random.nextInt(totalItensArray)
+        texto.setText(frases[numeroAleatorio])
     }
 }
